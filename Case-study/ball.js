71ddeed93c6eade3c7054aaa1ed9792b0488d5f6
herokillv1 <nguyentrunghieu.hha101196@gmail.com>
2020-06-28 15:05:42 +0700

    'taopaddle'

--- Case-study/ball.js
+++ Case-study/ball.js
@@ -4,6 +4,32 @@ var radius = 15;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var circle= new Circle(x, y, radius);
+var paddleHeight = 15;
+var paddleWidth = 150;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDown, false);
+document.addEventListener("keyup", keyUp, false);
+
+function keyDown(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUp(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
 
 function Circle(x, y, radius){
     this.x = x;
@@ -11,6 +37,14 @@ function Circle(x, y, radius){
     this.radius = radius;
     this.dx =5;
     this.dy =-5;
+}
+
+function paddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#000000";
+    ctx.fill();
+    ctx.closePath();
 }
 
 function createCircle(){
@@ -24,16 +58,33 @@ function createCircle(){
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     createCircle();
+    paddle();
 
     if(circle.x + circle.dx > canvas.width-radius || circle.x + circle.dx < radius) {
         circle.dx = -circle.dx;
     }
-    if(circle.y + circle.dy > canvas.height-radius||circle.y + circle.dy < radius) {
+    if(circle.y + circle.dy < radius) {
         circle.dy = -circle.dy;
+    }else if (circle.y + circle.dy > canvas.height-radius){
+        if (circle.x>paddleX && circle.x<paddleX+paddleWidth){
+            circle.dy = -circle.dy;
+        }else {
+            alert('GAME OVER');
+            document.location.reload();
+            clearInterval(interval);
+        }
     }
+
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
 
     circle.x += circle.dx*1.8;
     circle.y += circle.dy*1.3;
 }
 
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
