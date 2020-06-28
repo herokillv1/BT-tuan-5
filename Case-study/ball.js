9c67d607f7d7ba4fd933675fd34024a62b12fb88
herokillv1 <nguyentrunghieu.hha101196@gmail.com>
2020-06-28 15:59:57 +0700

    'taomonster'

--- Case-study/ball.js
+++ Case-study/ball.js
@@ -4,29 +4,39 @@ var radius = 15;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var circle= new Circle(x, y, radius);
+
 var paddleHeight = 15;
 var paddleWidth = 150;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
+var monster = [];
+var monsterRowCount = 5;
+var monsterColumnCount = 6;
+var monsterWidth = 150;
+var monsterHeight = 30;
+var monsterPadding = 32;
+var monsterOffsetTop = 30;
+var monsterOffsetLeft = 32;
+
 document.addEventListener("keydown", keyDown, false);
 document.addEventListener("keyup", keyUp, false);
 
 function keyDown(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
+    if(e.key == "ArrowRight") {
         rightPressed = true;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    else if( e.key == "ArrowLeft") {
         leftPressed = true;
     }
 }
 
 function keyUp(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
+    if( e.key == "ArrowRight") {
         rightPressed = false;
     }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
+    else if( e.key == "ArrowLeft") {
         leftPressed = false;
     }
 }
@@ -47,6 +57,29 @@ function paddle() {
     ctx.closePath();
 }
 
+for(var i=0; i<monsterColumnCount; i++) {
+    monster[i] = [];
+    for(var j=0; j<monsterRowCount; j++) {
+        monster[i][j] = { x: 0, y: 0 };
+    }
+}
+
+function drawMonster() {
+    for(var i=0; i<monsterColumnCount; i++) {
+        for(var j=0; j<monsterRowCount; j++) {
+            var monsterX = (i*(monsterWidth+monsterPadding))+monsterOffsetLeft;
+            var monsterY = (j*(monsterHeight+monsterPadding))+monsterOffsetTop;
+            monster[i][j].x = monsterX;
+            monster[i][j].y = monsterY;
+            ctx.beginPath();
+            ctx.rect(monsterX, monsterY, monsterWidth, monsterHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
 function createCircle(){
     ctx.beginPath();
     ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
@@ -55,10 +88,11 @@ function createCircle(){
     ctx.closePath();
 }
 
-function draw() {
+function move() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     createCircle();
     paddle();
+    drawMonster();
 
     if(circle.x + circle.dx > canvas.width-radius || circle.x + circle.dx < radius) {
         circle.dx = -circle.dx;
@@ -70,7 +104,7 @@ function draw() {
             circle.dy = -circle.dy;
         }else {
             alert('GAME OVER');
-            document.location.reload();
+            // document.location.reload();
             clearInterval(interval);
         }
     }
@@ -87,4 +121,4 @@ function draw() {
     circle.y += circle.dy*1.3;
 }
 
-var interval = setInterval(draw, 10);
+var interval = setInterval(move, 10);
