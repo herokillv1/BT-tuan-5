4d22ea6e5fc2f1a62e8e84c492f9cdcebbc676eb
herokillv1 <nguyentrunghieu.hha101196@gmail.com>
2020-06-29 12:00:45 +0700

    test

---
+++ Case-study/Code OOP/ball.js
@@ -0,0 +1,50 @@
+let Ball = function (x,y,dx,dy,radius,lives) {
+    this.x = x ;
+    this.y = y ;
+    this.dx = dx ;
+    this.dy = dy ;
+    this.radius = radius ;
+    this.lives = lives ;
+
+
+    this.drawBall = function () {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI,true);
+        ctx.fillStyle = "#ff3333";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    this.drawLives = function () {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = '#ececec';
+        ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+    }
+
+    this.moveBall = function () {
+        if(this.x + this.dx > canvas.width-this.radius || this.x + this.dx < this.radius) {
+            this.dx = -this.dx;
+        }
+        if(this.y + this.dy < this.radius) {
+            this.dy = -this.dy;
+        }else if (this.y + this.dy > canvas.height-this.radius){
+            if (this.x>paddle.paddleX-5 && this.x<paddle.paddleX+200+5){
+                this.dy = -this.dy;
+            }else {
+                lives--;
+                if (!lives){
+                    alert('NGHỈ HỌC BÀI ĐI CHO ĐỠ HẠI MẮT BẠN ƠI !!!');
+                    document.location.reload();
+                }else {
+                    this.x = canvas.width/2;
+                    this.y = canvas.height-15;
+                    this.dx = 5;
+                    this.dy = -5;
+                    paddle.paddleX = (canvas.width-200)/2;
+                }
+            }
+        }
+        this.x += this.dx*2;
+        this.y += this.dy*2;
+    }
+}
---
+++ Case-study/Code OOP/main.js
@@ -0,0 +1,36 @@
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+let ball = new Ball((canvas.width/2), (canvas.height-30),5,-5, 15,3);
+let paddle = new Paddle(15,200,(canvas.width-200)/2);
+let monster = new Monster(5,6,150,30,32,30,70,0);
+
+
+function playgame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ball.drawBall();
+    paddle.drawPaddle();
+    ball.drawLives();
+    monster.drawMonster();
+    monster.drawScore();
+    monster.collisionCheck();
+    ball.moveBall();
+    paddle.movePallde();
+    requestAnimationFrame(playgame);
+    document.getElementById("start").innerHTML = 'Lever Up' ;
+}
+
+
+function restart() {
+    document.location.reload();
+}
+
+
+function load() {
+    ball.drawBall();
+    paddle.drawPaddle();
+    monster.drawMonster();
+
+}
+ load();
+
+
